441aa0c5f55a54775450657825fd20e1bd8500d8
duc01 <duc011002@gmail.com>
2026-05-11 10:11:34 +0700

    chore: clean up scratch directory

--- database/scratch/search_sp_b767.ps1
+++
@@ -1,7 +0,0 @@
-$server = "dbserver.hycap.co.kr,5398"
-$database = "SmartFactoryV2"
-$user = "vinaadmin"
-$pass = "vina1234%6&8"
-
-$query = "SELECT OBJECT_NAME(id) AS SP_Name FROM syscomments WHERE text LIKE '%B767%' GROUP BY OBJECT_NAME(id)"
-sqlcmd -S $server -d $database -U $user -P $pass -Q $query -W -C
