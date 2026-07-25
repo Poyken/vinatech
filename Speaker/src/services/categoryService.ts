5345467b53413f219afa9ac2eb002ff1e4f36496
duc01 <duc011002@gmail.com>
2026-07-25 09:05:32 +0700

    update doc

--- Speaker/src/services/categoryService.ts
+++
@@ -1,8 +0,0 @@
-import { dataService } from '../lib/dataService';
-import { Category } from '../lib/types';
-
-export const categoryService = {
-  async getCategories(): Promise<Category[]> {
-    return dataService.getCategories();
-  }
-};
